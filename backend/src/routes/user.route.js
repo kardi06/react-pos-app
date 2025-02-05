d53991f395c3008f7dd46849b7bc803bbb432673
kardi.ib <kardi.ib@gmail.com>
2025-02-05 15:30:57 +0700

    update user login token

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, updateUser } from "../controllers/user.controller.js";
+import { autenticate } from "../controllers/error.controller.js";
 
 const userRouter = Router();
 
 userRouter.post("/users", createUser);
+userRouter.put("/users/:id", autenticate, updateUser);
 
 export default userRouter;
