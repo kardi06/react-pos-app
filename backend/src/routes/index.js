0e6796663b9580f826a18e3479aacf1eaceaf32c
kardi.ib <kardi.ib@gmail.com>
2025-02-08 23:53:24 +0700

    category

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import userRouter from "./user.route.js";
+import categoryRoute from "./category.route.js";
 
 const router = Router();
 
 router.use("/api", userRouter);
+router.use("/api", categoryRoute);
+router.use("*", (req, res) => res.status(404).json({ message: "not found" }));
 
 export default router;
