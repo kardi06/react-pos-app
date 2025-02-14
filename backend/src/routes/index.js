c82cd39b1abb625d068901fd8ee80e2c3e536851
kardi.ib <kardi.ib@gmail.com>
2025-02-14 10:01:25 +0700

    backend cart

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,12 +3,16 @@ import { Router } from "express";
 import userRouter from "./user.route.js";
 import categoryRoute from "./category.route.js";
 import supplierRouter from "./supplier.route.js";
+import productRouter from "./product.route.js";
+import cartRouter from "./cart.route.js";
 
 const router = Router();
 
 router.use("/api", userRouter);
 router.use("/api", categoryRoute);
 router.use("/api", supplierRouter);
+router.use("/api", productRouter);
+router.use("/api", cartRouter);
 router.use("*", (req, res) => res.status(404).json({ message: "not found" }));
 
 export default router;
