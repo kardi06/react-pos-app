3e7d70029d48ee1feaf8479de462eb97cccdbacd
kardi.ib <kardi.ib@gmail.com>
2025-02-15 22:04:57 +0700

    backend order Return and Purchase

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,8 @@ import supplierRouter from "./supplier.route.js";
 import productRouter from "./product.route.js";
 import cartRouter from "./cart.route.js";
 import orderRouter from "./order.route.js";
+import orderReturnRouter from "./orderReturn.route.js";
+import purchaseRouter from "./purchase.route.js";
 
 const router = Router();
 
@@ -15,6 +17,8 @@ router.use("/api", supplierRouter);
 router.use("/api", productRouter);
 router.use("/api", cartRouter);
 router.use("/api", orderRouter);
+router.use("/api", orderReturnRouter);
+router.use("/api", purchaseRouter);
 router.use("*", (req, res) => res.status(404).json({ message: "not found" }));
 
 export default router;
