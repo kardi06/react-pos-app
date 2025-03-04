f42e0941560f529404782b52aac04ed445e08c25
kardi.ib <kardi.ib@gmail.com>
2025-03-04 10:00:18 +0700

    sales Return

--- frontend/src/auth/RouteNavigation.jsx
+++ frontend/src/auth/RouteNavigation.jsx
@@ -17,7 +17,7 @@ import EditProduct from "../components/product/EditProduct.jsx";
 import ListSales from "../components/sales/ListSales.jsx";
 import OrderSend from "../components/sales/OrderSend.jsx";
 import ListSalesHistory from "../components/salesHistory/ListSalesHistory.jsx";
-// import SalesReturn from "../components/salesHistory/SalesReturn.jsx";
+import SalesReturn from "../components/salesHistory/SalesReturn.jsx";
 // import ListPurchase from "../components/purchase/ListPurchase.jsx";
 // import AddPurchase from "../components/purchase/AddPurchase.jsx";
 // import PrintPurchase from "../components/purchase/PrintPurchase.jsx";
@@ -53,7 +53,7 @@ const RouteNavigation = () => {
               <Route path="/orders/:id" element={<OrderSend />} />
               {/* sales History */}
               <Route path="/sales-history" element={<ListSalesHistory />} />
-              {/* <Route path="/sales-return/:id" element={<SalesReturn />} /> */}
+              <Route path="/sales-return/:id" element={<SalesReturn />} />
               {/* purchase */}
               {/* <Route path="/purchase" element={<ListPurchase />} />
               <Route path="/purchase/add" element={<AddPurchase />} />
