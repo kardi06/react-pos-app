a44803f2c8c43383e98ba174d6e62809c09e9ab0
kardi.ib <kardi.ib@gmail.com>
2025-03-19 16:42:10 +0700

    sales report

--- frontend/src/auth/RouteNavigation.jsx
+++ frontend/src/auth/RouteNavigation.jsx
@@ -23,7 +23,7 @@ import AddPurchase from "../components/purchase/AddPurchase.jsx";
 import PrintPurchase from "../components/purchase/PrintPurchase.jsx";
 import SupplierReport from "../components/report/supplier/SupplierReport.jsx";
 import ProductReport from "../components/report/product/ProductReport.jsx";
-// import SalesReport from "../components/report/sales/SalesReport.jsx";
+import SalesReport from "../components/report/sales/SalesReport.jsx";
 // import PurchaseReport from "../components/report/purchase/PurchaseReport.jsx";
 
 const RouteNavigation = () => {
@@ -61,7 +61,7 @@ const RouteNavigation = () => {
               {/* report */}
               <Route path="/supplier-report" element={<SupplierReport />} />
               <Route path="/product-report" element={<ProductReport />} />
-              {/* <Route path="/sales-report" element={<SalesReport />} /> */}
+              <Route path="/sales-report" element={<SalesReport />} />
               {/* <Route path="/purchase-report" element={<PurchaseReport />} /> */}
               {/* page notfound */}
               <Route path="*" element={<NoPage />} />
